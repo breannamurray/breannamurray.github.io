00a2ecee01e26c3ee832beaeebc016e655dd332d
Breanna Murray <breannamurray@purdue.edu>
2016-09-04 23:42:03 -0500

    contact form

--- mail.php
+++ mail.php
@@ -1,4 +1,29 @@
-
+<?php
+    if(empty($_POST) === false) {
+        $errors = array();
+        
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        
+        if (empty($name) === true || empty($email) === true || empty($message) === true) {
+            $errors[] = 'Name, email and message are required!';
+        }
+        else {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+            $errors[] = 'That\'s not a valid email address';
+            }
+            if (ctype_alpha($name) === false) {
+                $errors[] = 'Name must only contain letters';
+            }
+        }
+        if(empty($errors) === true) {
+            mail('hmp260@my.utsa.edu', 'Contact form', $message, 'From: ' . $email);
+            header('Location: index.html?sent');
+            exit();
+        }
+    }
+?>
 
 <!DOCTYPE html>
 <html>
